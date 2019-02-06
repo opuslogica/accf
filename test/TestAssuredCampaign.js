5ae30c772b89cf1755155dc1b8684f0828db5be8
Alireza Rafiei <mail@rafiei.net>
2019-02-06 13:00:07 -0800

    ent profit less than the target amount test

--- test/TestAssuredCampaign.js
+++ test/TestAssuredCampaign.js
@@ -1,6 +1,8 @@
 const AssuredCampaign = artifacts.require("./AssuredCampaign.sol");
+const tryCatch = require("./exceptions").tryCatch;
+const errTypes = require("./exceptions").errTypes;
 
-const params = (start, end, target, profit, minAmount, recepient) => {
+const params = ({start, end, target, profit, minAmount, recepient}) => {
   let current_time = Date.now();
   return [
     start || current_time - 120,
@@ -14,11 +16,28 @@ const params = (start, end, target, profit, minAmount, recepient) => {
 
 contract("Testing campaign", async accounts => {
 
-  it("should compile correctly", async () => {
-    assert.exists(await AssuredCampaign.new(...params()));
+  it("should deploy without an error", async () => {
+    assert.exists(await AssuredCampaign.new(...params({})));
+  });
+
+  it("should have a far enough start time", async () => {
+    assert.isOk(await AssuredCampaign.new(...params({start: Date.now()})));
+  });
+
+  it("should an entrepreneur's profit less than the target raising amount", async () => {
+    await tryCatch(
+      AssuredCampaign.new(...params({profit:10, target:10})),
+      errTypes.revert
+    );
+    await tryCatch(
+      AssuredCampaign.new(...params({profit:11, target:10})),
+      errTypes.revert
+    );
+    assert.isOk(await AssuredCampaign.new(...params({profit:9, target:10})));
   });
 
 });
 
 
 
+// 2549405445, 8549405445, 50, 10, 2, 0x31119260c0Bd3a8Ad822878B687efc3AFB60B603
