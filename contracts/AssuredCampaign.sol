f769c37f7af296316fd53241fff329106300da01
Alireza Rafiei <mail@rafiei.net>
2019-02-06 13:29:45 -0800

    adding solium, formatting and error messages

--- contracts/AssuredCampaign.sol
+++ contracts/AssuredCampaign.sol
@@ -23,28 +23,28 @@ contract AssuredCampaign {
                 uint256 minAmount, address payable recepient)
     public
     {
-       require(end - start > 5 minutes);
-       require(start > now + 1 minutes);
-       require(profit < target);
-    startTime = start;
-    deadline = end;
-    entProfitAmount = profit;
-    targetAmount = target;
-    entAccount = msg.sender;
-    recepientAccount = recepient;
-    contribMinAmount = minAmount;
+        require(end - start > 5 minutes, "The deadline should be at least 5 minutes away from the start time");
+        require(start > now + 1 minutes, "The start time should at least be a minute from now");
+        require(profit < target, "Entrepreneur's profit should be less than the target raising amount");
+        startTime = start;
+        deadline = end;
+        entProfitAmount = profit;
+        targetAmount = target;
+        entAccount = msg.sender;
+        recepientAccount = recepient;
+        contribMinAmount = minAmount;
     }
 
     function pledge(uint256 amount)
     public
     payable
     {
-        require(amount == msg.value);
-        require(msg.sender != entAccount);
-        require(amount > contribMinAmount);
-       require(startTime < now);
-       require(now < deadline);
-        require(balanceOf[entAccount] >= (targetAmount / contribMinAmount + 1) * monetaryIndivisibleAmount);
+        require(amount == msg.value, "Transaction doesn't have enough value as the claimed amount");
+        require(msg.sender != entAccount, "Entrepreneur's can't pledge");
+        require(amount > contribMinAmount, "Pledging amount should be greater than contribMinAmount");
+        require(startTime < now, "You can't pledge before the start time");
+        require(now < deadline, "You can't pledge after the deadline");
+        require(balanceOf[entAccount] >= (targetAmount / contribMinAmount + 1) * monetaryIndivisibleAmount, "Entrepreneur doesn't have enough staked to assure your pledge's profit in case of a failed campaign");
         balanceOf[msg.sender] = amount;
         receivedRefunds[msg.sender] = false;
         amountRaised += amount;
@@ -54,30 +54,29 @@ contract AssuredCampaign {
     public
     payable
     {
-      require(now < startTime);
-      require(amount == msg.value);
-      require(entAccount == msg.sender);
-      balanceOf[entAccount] += msg.value;
+        require(now < startTime, "Entrepreneur can't stake after the start time");
+        require(amount == msg.value, "Transaction doesn't have enough value as the claimed amount");
+        require(entAccount == msg.sender, "Only the entrepreneur can stake");
+        balanceOf[entAccount] += msg.value;
     }
 
     function refund()
     public
     {
-      require(now > deadline);
-      require(msg.sender != entAccount);
-      require(!receivedRefunds[msg.sender]);
-      require(address(this).balance <= targetAmount + balanceOf[entAccount]);
-      receivedRefunds[msg.sender] = true;
-      msg.sender.transfer(balanceOf[msg.sender] * (1 + balanceOf[entAccount] / amountRaised)
-        );
+        require(now > deadline, "Can't request a refund prior to the deadline");
+        require(msg.sender != entAccount, "Entrepreneur's can't take back their stake");
+        require(!receivedRefunds[msg.sender], "Must have pledged something to refund");
+        require(address(this).balance <= targetAmount + balanceOf[entAccount], "Can't get a refund if the target amount is reached");
+        receivedRefunds[msg.sender] = true;
+        msg.sender.transfer(balanceOf[msg.sender] * (1 + balanceOf[entAccount] / amountRaised));
     }
 
     function satisfied_contract_termination_process()
     public
     {
-        require(msg.sender == entAccount);
-        require(address(this).balance >= targetAmount + balanceOf[entAccount]);
-       require(now > deadline);
+        require(msg.sender == entAccount, "Only Entrepreneur's can distribute raised funds");
+        require(address(this).balance >= targetAmount + balanceOf[entAccount], "Can't distribute raised funds if the target amount isn't reached");
+        require(now > deadline, "Can't distribute raised funds before the deadline");
         uint256 entShare = balanceOf[entAccount] + entProfitAmount;
         uint256 recepientShare = amountRaised - entShare;
         if (!entProfitted) {
